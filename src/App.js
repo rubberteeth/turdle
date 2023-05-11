7dd5f217aaa8c6a7d14f95f1622c2168eb7f3453
Mike Melchior <mike.melchior@outlook.com>
2023-05-11 16:55:16 -0400

    move openMenu function to app, render menu component and conditionally game

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
 import './index.css';
-import WelcomePage from "./Components/WelcomePage";
 import { ThemeProvider } from "./ThemeContext";
+import WelcomePage from "./Components/WelcomePage";
+import Menu from "./Components/Menu";
+import Game from "./Components/Game";
 
 
 function App() {
   const [user, setUser] = useState(null);
 
+  function openMenu() {
+    const menu = document.querySelector('.menu')
+    menu.classList.remove('hidden')
+    menu.className = 'menu bg-gray-300 absolute right-0 h-full w-2/5 z-10 translate-x-0';
+  }
+
   return (
     <ThemeProvider>
-      <div className="app min-h-screen bg-yellow-50 flex justify-center items-center">
+      <div 
+        className="app min-h-screen bg-yellow-50 flex justify-center items-center"
+      >
+        <Menu setUser={setUser}/>
         {user
         ?
-          null
+          <Game openMenu={openMenu}/>
         :
-         <WelcomePage 
-          setUser={setUser}
-        />
+        <>
+          <WelcomePage 
+            setUser={setUser}
+            openMenu={openMenu}
+          />
+        </>
+         
         }
       </div>
     </ThemeProvider>
