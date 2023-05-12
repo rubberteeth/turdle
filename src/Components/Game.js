9c3b343e811b51b0990f972ea50b78b87d4bef8c
Mike Melchior <mike.melchior@outlook.com>
2023-05-12 11:12:36 -0400

    use dark theme context, add ability to close menu with 'click off'

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import MenuButton from './MenuButton'
+import { useTheme } from '../ThemeContext'
 
-export default function Game( {openMenu } ) {
+export default function Game( { openMenu, closeMenu } ) {
+
+  const darkTheme = useTheme()
+
+  const styles = {
+    backgroundColor : darkTheme ? 'rgb(114, 43, 0)' : 'rgb(255, 251, 235)',
+    color: darkTheme ? '#eee' : '#111',
+  }
+
   return (
-    <div>
+    <>
       <MenuButton openMenu={openMenu}/>
-      Insert Game Here
-    </div>
+      <div
+        onClick={closeMenu}
+        style={styles}
+        className='game w-screen min-h-screen flex items-center justify-center'
+      >
+        Insert Game Here
+      </div>
+    </>
+    
   )
 }
