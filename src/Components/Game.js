83b197e2104c9af2b88466f5791c7e2f5168942f
Mike Melchior <mike.melchior@outlook.com>
2023-05-17 14:22:19 -0400

    render keyboard, adjust styles, remove menu button into header

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,27 +1,36 @@
 import React from 'react'
-import MenuButton from './MenuButton'
 import { useTheme } from '../ThemeContext'
 
-export default function Game( { openMenu, closeMenu } ) {
+import Keyboard from './Keyboard'
+
+// import Words from './Words'
+
+export default function Game( { closeMenu } ) {
 
   const darkTheme = useTheme()
 
   const styles = {
-    backgroundColor : darkTheme ? 'rgb(114, 43, 0)' : 'rgb(255, 251, 235)',
+    backgroundColor : darkTheme ? 'rgb(110 125 140)' : 'rgb(245, 245, 245)',
     color: darkTheme ? '#eee' : '#111',
   }
 
   return (
-    <>
-      <MenuButton openMenu={openMenu}/>
+    <div
+    className='game w-screen flex-grow flex flex-col'
+    style={styles}
+    onClick={closeMenu}
+    >
       <div
-        onClick={closeMenu}
-        style={styles}
-        className='game w-screen min-h-screen flex items-center justify-center'
+        className="game-screen w-full bg-blue-400 flex-grow"
       >
-        Insert Game Here
+
+        {/* <Words /> */}
+
+
+        
       </div>
-    </>
+      <Keyboard />
+    </div>
     
   )
 }
