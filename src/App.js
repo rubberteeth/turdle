7f3adb69bf592e1b039f426735cd375865d5635a
Mike Melchior <mike.melchior@outlook.com>
2023-05-17 14:18:51 -0400

    render how to play modal and header, adjust logic for opening/closing menus

--- src/App.js
+++ src/App.js
@@ -4,48 +4,65 @@ import { ThemeProvider } from "./ThemeContext";
 import WelcomePage from "./Components/WelcomePage";
 import Menu from "./Components/Menu";
 import Game from "./Components/Game";
+import HowToPlayModal from "./Components/HowToPlayModal";
+import Header from "./Components/Header";
 
 
 function App() {
   const [user, setUser] = useState(null);
 
+
   function openMenu() {
     const menu = document.querySelector('.menu')
     menu.classList.remove('hidden')
-    menu.className = 'menu bg-gray-300 absolute right-0 h-full w-2/5 z-10 translate-x-0';
+    menu.className = 'menu bg-gray-300 absolute right-0 top-0 h-screen w-2/5 z-20 translate-x-0';
   }
 
   function closeMenu() {
     const menu = document.querySelector('.menu')
-    menu.className = 'menu bg-gray-300 absolute right-0 w-2/5 h-full z-10 translate-x-full';
+    menu.className = 'menu bg-gray-300 absolute right-0 top-0 w-2/5 h-screen z-20 translate-x-full';
   }
 
   function openHowTo() {
     const dialog = document.querySelector('.page-1');
+    const pageTwo = document.querySelector('.page-2');
+    try {
+      dialog.close();
+      pageTwo.close();
+    } catch(e) {
+      console.log(e)
+    }
+
     dialog.showModal();
     dialog.classList.add('show');
   };
 
+
+
   return (
     <ThemeProvider>
       <div 
-        className="app min-h-screen bg-yellow-50 flex justify-center items-center"
+        className="app h-full flex flex-col justify-center items-center"
       >
+        <Header 
+          user={user}
+          openMenu={openMenu}
+        />
         <Menu 
           setUser={setUser} 
+          openHowTo={openHowTo}
           closeMenu={closeMenu}
         />
+        <HowToPlayModal />
         {user
         ?
           <Game 
-            openMenu={openMenu}
             closeMenu={closeMenu}
           />
         :
           <WelcomePage 
             setUser={setUser}
             openHowTo={openHowTo}
-            openMenu={openMenu}
             closeMenu={closeMenu}
           />
         }
