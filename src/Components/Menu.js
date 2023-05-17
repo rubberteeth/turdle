6311ed77bb9c855b6c5d80eac54e2f4fbadf57bf
Mike Melchior <mike.melchior@outlook.com>
2023-05-17 14:23:42 -0400

    change styling

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import { useTheme, useThemeUpdate } from '../ThemeContext';
 
-export default function Menu( { setUser } ) {
+export default function Menu( { setUser, openHowTo, closeMenu } ) {
 
   const darkTheme = useTheme()
   const toggleTheme = useThemeUpdate();
 
   const styles = {
     backgroundColor : darkTheme ? 'rgb(75, 85, 99)' : 'rgb(220, 220, 220)',
-    color: darkTheme ? '#eee' : '#000'
+    color: darkTheme ? '#eee' : '#050505',
   }
 
   const buttonStyles = {
-    borderColor: darkTheme ? '#e5e7eb' : '#323232'
-  }
-
-  function closeMenu() {
-    const menu = document.querySelector('.menu')
-    menu.className = 'menu bg-gray-300 absolute right-0 w-2/5 h-full z-10 translate-x-full';
+    borderColor: darkTheme ? '#e5e7eb' : '#323232',
+    color: darkTheme ? '#fefefe' : '#050505',
+    padding: '5px',
   }
 
   function logOut() {
@@ -27,7 +24,7 @@ export default function Menu( { setUser } ) {
   return (
     <nav 
       style={styles}
-      className='menu bg-gray-300 absolute right-0 w-2/5 h-full z-10 
+      className='menu bg-gray-300 absolute top-0 right-0 w-2/5 h-screen z-20 
       translate-x-full font-cinzel'
     >
       <div 
@@ -39,19 +36,40 @@ export default function Menu( { setUser } ) {
       </div>
 
       <ul className='w-full mt-16 p-6 flex flex-col gap-4'>
+
         <li className='flex w-full'>
-          
           <label className='switch w-full flex items-center' htmlFor="darkmode"> 
             <p className='text-xl'>Dark Mode</p>
             <input type="checkbox" name='darkmode' id='darkmode' onClick={toggleTheme}/>
             <span className='slider round'></span>
           </label>
         </li>
+
+        <li>
+          <button
+            style={buttonStyles}
+            onClick={() => {
+              closeMenu()
+              openHowTo()
+            }}
+            className='border-2 rounded-md w-full bg-gray-400 hover:scale-105'
+          >
+            How To Play
+          </button>
+        </li>
+
+
+
+
+
         <li className='flex justify-center'>
           <button 
             style={buttonStyles}
-            onClick={logOut}
-            className='border-2 rounded-md w-full'
+            onClick={() => {
+              closeMenu()
+              logOut()
+            }}
+            className='border-2 rounded-md w-full bg-gray-400 hover:scale-105'
           >
             Sign Out
           </button>
