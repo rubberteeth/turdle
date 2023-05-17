535ff56e75e45503e49cfe5782b84b3bee6596b5
Mike Melchior <mike.melchior@outlook.com>
2023-05-17 14:20:09 -0400

    begin creating quality words list for use in game

---
+++ src/Assets/Data/qualityWords.js
@@ -0,0 +1,29 @@
+export const qualityWordsArray = [
+  // quality words sorted :
+  'abbey', 'abbot', 'abide', 'abner', 'abode', 'abort', 'about', 'above', 'abuse', 'abyss', 'acorn', 'acres', 'actin', 'actor', 
+  'acute', 'adama', 'adams', 'adapt', 'adela', 'adele', 'adieu', 'adios', 'admit', 'adolf', 'adopt', 'adore', 'adult', 'after', 
+  'again', 'agent', 'aging', 'agony', 'agree', 'ahead', 'aired', 'aisle', 'alarm', 'album', 'alert', 'algae', 'alias', 'alibi', 
+  'alien', 'alike', 'aline', 'alive', 'allah', 'allen', 'alley', 'allow', 'aloha', 'alone', 'along', 'aloud', 'alpha', 'altar', 
+  'alter', 'amaze', 'amber', 'amigo', 'amish', 'amour', 'amuse', 'andes', 'angel', 'anger', 'angle', 'angry', 'angus', 'ankle', 
+  'annoy', 'apart', 'apple', 'apply', 'april', 'apron', 'arena', 'argue', 'arise', 'armed', 'armor', 'aroma', 'array', 'arrow', 
+  'arson', 'aryan', 'bacon', 'badge', 'badly', 'bagel', 'baggy', 'baked', 'baker', 'baldy', 'banal', 'banjo', 'banks', 'barge', 
+  'baron', 'based', 'basic', 'basil', 'basin', 'basis', 'batch', 'bathe', 'baton', 'begin', 'beige', 'being', 'belle', 'belly', 
+  'below', 'bench', 'bends', 'benny', 'berry', 'betel', 'bible', 'bijou', 'biker', 'billy', 'bimbo', 'bingo', 'birch', 'birth', 
+  'bison', 'bitch', 'bitty', 'black', 'blade', 'blair', 'blake', 'blame', 'bland', 'blank', 'blast', 'blaze', 'bleak', 'bleed', 
+  'bleep', 'blend', 'bless', 'blind', 'bling', 'blink', 'bliss', 'blitz', 'block', 'bloke', 'blond', 'blood', 'bloom', 'blown', 
+  'blues', 'bluff', 'blunt', 'blush', 'board', 'boast', 'bobby', 'bogus', 'boner', 'bones', 'bonny', 'bonus', 'booby', 'boone', 
+  'boost', 'booth', 'booty', 'booze', 'bored', 'bosom', 'bossy', 'botox', 'bound', 'bowed', 'bowel', 'bowie', 'bowls', 'boxed', 
+  'boxer', 'boyle', 'brace', 'brady', 'bragg', 'brain', 'brake', 'brand', 'brass', 'braun', 'brave', 'bravo', 'brawl', 'bread', 
+  'break', 'breed', 'brent', 'bribe', 'brick', 'bride', 'brief', 'bring', 'brink', 'brits', 'britt', 'broad', 'broke', 'bronx', 
+  'brood', 'brook', 'broom', 'broth', 'brown', 'bruce', 'bruno', 'brush', 'brute', 'bryan', 'buddy', 'budge', 'buggy', 'bugle', 
+  'build', 'built',
+
+
+  // Quality checked 1250 words so far from words data
+
+  
+
+
+]
+
+
