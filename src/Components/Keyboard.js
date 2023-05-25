e8c39b8572fbabfbfab6273dc0de1fdfac2f70b9
Mike Melchior <mike.melchior@outlook.com>
2023-05-25 10:25:37 -0400

    remove some functionality and place into Game component. add specific letter class names to keys for easy targeting

--- src/Components/Keyboard.js
+++ src/Components/Keyboard.js
@@ -1,90 +1,19 @@
 import React from 'react'
 import backspace from '../Assets/Pictures/backspace.png'
+import { useTheme } from '../ThemeContext'
 
 
 
-export default function Keyboard( { currentGuess, setCurrentGuess, activeRow, setActiveRow, tempWord } ) {
+export default function Keyboard( { 
+  handleLetterInput,
+  handleGuess,
+  handleBackspace
+  } ) {
 
-  // 
-  const tempWordList = [
-    'games',
-    'gamer',
+  const darkTheme = useTheme()
 
-  ]
-  // 
-
-
-
-  function handleBackspace() {
-    if (!currentGuess.length) return ;
-    setCurrentGuess(prev => {
-      return [...prev.slice(0, prev.length - 1)]
-    })
-  }
-
-  function handleLetterInput(letter) {
-    if (currentGuess.length === 5) return;
-    setCurrentGuess(prev => [...prev, letter])
-  } 
-
-  // function storeGuessDataLocally() {
-
-  // }
-
-  function lowerCaseGuess() {
-    return currentGuess.join('').toLowerCase();
-  }
-
-  function guessIsInWordList() {
-    if (tempWordList.indexOf(lowerCaseGuess()) === -1) return false
-    else return true
-  }
-
-  function animateWrongGuess() {
-    let guessRow = document.querySelector(`.guess-row-${activeRow}`);
-    guessRow.classList.add('shake')
-    setTimeout(() => {
-      guessRow.classList.remove('shake')
-    }, 400)
-  }
-
-  function showNotValidWordWarning() {
-    document.querySelector('.word-warning').classList.remove('opacity-0');
-    setTimeout(() => {
-      document.querySelector('.word-warning').classList.add('opacity-0');
-    }, 700);
-  }
-
-  function handleGuessSubmit() {
-    // if not a full word return
-    if (currentGuess.length !== 5) return
-    
-    // if guess isn't in word list
-    if (guessIsInWordList() === false) {
-      animateWrongGuess()
-      showNotValidWordWarning()
-      return
-    }
-    
-    
-    // if guess is correct show stats and end game loop
-    if (lowerCaseGuess() === tempWord.toLowerCase()) {
-      // win game
-      alert('you win') 
-
-      // show stats
-
-      // maybe set a game won state, or check db for some kind of game data
-      // to determine if game has been won and remove all click events in that case
-      return
-    }
-
-    // if full word, not correct word, and guess is in word list
-    if (activeRow < 6) {
-      setCurrentGuess([])
-      setActiveRow(prev => prev + 1)
-    }
-    
+  const keyStyles = {
+    backgroundColor: darkTheme ? 'rgb(75, 85, 99)' : 'rgb(220, 220, 220)'
   }
 
   
@@ -92,47 +21,49 @@ export default function Keyboard( { currentGuess, setCurrentGuess, activeRow, se
   return (
     <div className='keyboard w-screen flex flex-col gap-2 self-end items-center pb-2'>
       <div className='row-1 flex gap-0.5'>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>Q</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>W</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>E</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>R</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>T</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>Y</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>U</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>I</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>O</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>P</div>
+        <div style={keyStyles} className='letter q' onClick={(e) => {handleLetterInput(e.target.textContent)}}>Q</div>
+        <div style={keyStyles} className='letter w' onClick={(e) => {handleLetterInput(e.target.textContent)}}>W</div>
+        <div style={keyStyles} className='letter e' onClick={(e) => {handleLetterInput(e.target.textContent)}}>E</div>
+        <div style={keyStyles} className='letter r' onClick={(e) => {handleLetterInput(e.target.textContent)}}>R</div>
+        <div style={keyStyles} className='letter t' onClick={(e) => {handleLetterInput(e.target.textContent)}}>T</div>
+        <div style={keyStyles} className='letter y' onClick={(e) => {handleLetterInput(e.target.textContent)}}>Y</div>
+        <div style={keyStyles} className='letter u' onClick={(e) => {handleLetterInput(e.target.textContent)}}>U</div>
+        <div style={keyStyles} className='letter i' onClick={(e) => {handleLetterInput(e.target.textContent)}}>I</div>
+        <div style={keyStyles} className='letter o' onClick={(e) => {handleLetterInput(e.target.textContent)}}>O</div>
+        <div style={keyStyles} className='letter p' onClick={(e) => {handleLetterInput(e.target.textContent)}}>P</div>
       </div>
       <div className='spaced-row'>
         <div className='row-2 flex gap-0.5'>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>A</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>S</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>D</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>F</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>G</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>H</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>J</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>K</div>
-          <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>L</div>
+          <div style={keyStyles} className='letter a' onClick={(e) => {handleLetterInput(e.target.textContent)}}>A</div>
+          <div style={keyStyles} className='letter s' onClick={(e) => {handleLetterInput(e.target.textContent)}}>S</div>
+          <div style={keyStyles} className='letter d' onClick={(e) => {handleLetterInput(e.target.textContent)}}>D</div>
+          <div style={keyStyles} className='letter f' onClick={(e) => {handleLetterInput(e.target.textContent)}}>F</div>
+          <div style={keyStyles} className='letter g' onClick={(e) => {handleLetterInput(e.target.textContent)}}>G</div>
+          <div style={keyStyles} className='letter h' onClick={(e) => {handleLetterInput(e.target.textContent)}}>H</div>
+          <div style={keyStyles} className='letter j' onClick={(e) => {handleLetterInput(e.target.textContent)}}>J</div>
+          <div style={keyStyles} className='letter k' onClick={(e) => {handleLetterInput(e.target.textContent)}}>K</div>
+          <div style={keyStyles} className='letter l' onClick={(e) => {handleLetterInput(e.target.textContent)}}>L</div>
         </div>
         
       </div>
-      <div className='row-3 flex gap-0.5 w-full px-2'>
+      <div className='row-3 flex gap-0.5 w-full px-1'>
         <div 
+          style={keyStyles}
           className='enter'
-          onClick={handleGuessSubmit}
+          onClick={handleGuess}
         >
           ENTER
         </div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>Z</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>X</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>C</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>V</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>B</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>N</div>
-        <div className='letter' onClick={(e) => {handleLetterInput(e.target.textContent)}}>M</div>
+        <div style={keyStyles} className='letter z' onClick={(e) => {handleLetterInput(e.target.textContent)}}>Z</div>
+        <div style={keyStyles} className='letter x' onClick={(e) => {handleLetterInput(e.target.textContent)}}>X</div>
+        <div style={keyStyles} className='letter c' onClick={(e) => {handleLetterInput(e.target.textContent)}}>C</div>
+        <div style={keyStyles} className='letter v' onClick={(e) => {handleLetterInput(e.target.textContent)}}>V</div>
+        <div style={keyStyles} className='letter b' onClick={(e) => {handleLetterInput(e.target.textContent)}}>B</div>
+        <div style={keyStyles} className='letter n' onClick={(e) => {handleLetterInput(e.target.textContent)}}>N</div>
+        <div style={keyStyles} className='letter m' onClick={(e) => {handleLetterInput(e.target.textContent)}}>M</div>
         
         <div 
+          style={keyStyles}
           className='backspace'
           onClick={handleBackspace}  
         >
