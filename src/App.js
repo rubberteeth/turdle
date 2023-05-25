719f04c669057257f9960e6adf44e40bcee1afbd
Mike Melchior <mike.melchior@outlook.com>
2023-05-25 10:22:02 -0400

    add firebase functions, move some functions out of components into app that are for opening and closing dialogs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import './index.css';
 import { ThemeProvider } from "./ThemeContext";
+
+
 import WelcomePage from "./Components/WelcomePage";
 import Menu from "./Components/Menu";
 import Game from "./Components/Game";
@@ -8,9 +10,85 @@ import HowToPlayModal from "./Components/HowToPlayModal";
 import Header from "./Components/Header";
 
 
+import { firebaseConfig } from "./firebaseConfig";
+import { initializeApp } from 'firebase/app'
+
+import { 
+  collection, 
+  addDoc, 
+  serverTimestamp, 
+  getFirestore, 
+  doc,
+  getDoc,
+  updateDoc,
+  setDoc,
+  } from 'firebase/firestore'
+  
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import SignUp from "./Components/SignUp";
+
+
+async function addUserToDatabase(user) {
+  const usersRef = collection(getFirestore(), "users");
+  await setDoc(doc(usersRef, `${user}`), {
+    user: `${user}`, 
+    city: "whitby", 
+    country: "canada",
+    timeCreated: serverTimestamp()
+  });
+}
+
+
+async function getUserFromDatabase(user) {
+  const docRef = doc(getFirestore(), "users", `${user}`);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    console.log("Document data:", docSnap.data());
+  } else {
+    console.log("No such document!");
+  }
+  // get date/time from server timestamp
+  console.log(new Date(parseInt(docSnap.data().timeCreated.seconds.toString() + '000')))
+  console.log(window.navigator)
+}
+
+
+function createUser(email, password) {
+  createUserWithEmailAndPassword(getAuth(), email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user)
+    })
+    .catch((error) => {
+      console.log(error.code, error.message)
+    });
+}
+
+function localStorageGuestDataKey(userID) {
+  if (localStorage.getItem('turdle-data-key')) {
+    console.log(localStorage.getItem('turdle-data-key'))
+  } else {
+    localStorage.setItem('turdle-data-key', JSON.stringify(`${userID}`))
+  }
+}
+
+
 function App() {
   const [user, setUser] = useState(null);
+  const [dataKey, setDataKey] = useState('');
 
+
+
+
+  function getKeyFromStorage() {
+    setDataKey(() => JSON.parse(localStorage.getItem('turdle-data-key')));
+  }
+  
+
+
+
+
+  // Menu and Dialog box functions;
 
   function openMenu() {
     const menu = document.querySelector('.menu')
@@ -37,6 +115,33 @@ function App() {
     dialog.classList.add('show');
   };
 
+  function closeHowTo() {
+    const pageTwo = document.querySelector('.page-2');
+    pageTwo.classList.remove('show');
+    setTimeout(() => {
+      pageTwo.close();
+    }, 500);
+  };
+
+  function openSignUp() {
+    const signUp = document.querySelector('.sign-up');
+    try {
+      signUp.close();
+    } catch (e) {
+      console.log(e);
+    } 
+    signUp.showModal();
+    signUp.classList.add('show');
+  }
+
+  function closeSignUp() {
+    const signUp = document.querySelector('.sign-up');
+    signUp.classList.remove('show');
+    setTimeout(() => {
+      signUp.close()
+    }, 500);
+  }
+
 
 
   return (
@@ -53,7 +158,18 @@ function App() {
           openHowTo={openHowTo}
           closeMenu={closeMenu}
         />
-        <HowToPlayModal />
+        <HowToPlayModal
+          closeHowTo={closeHowTo}
+        />
+        <SignUp 
+          closeSignUp={closeSignUp}
+        />
+
+
+        <button className="p-4 border-2 w-full"
+        onClick={() => {createUser('mike.melchior@outlook.com', '123456')}}>TEST ADD USER</button>
+
+
         {user
         ?
           <Game 
@@ -64,6 +180,7 @@ function App() {
             setUser={setUser}
             openHowTo={openHowTo}
             closeMenu={closeMenu}
+            openSignUp={openSignUp}
           />
         }
       </div>
@@ -72,3 +189,8 @@ function App() {
 };
 
 export default App;
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
